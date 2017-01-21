0bbb3baea9124952d74a3b14ec5ce585c4b03880
baggerest <baggerest@yahoo.com.tw>
2017-01-21 21:55:10 +0800

    更改每行開頭的空格

--- function.php
+++ function.php
@@ -1,13 +1,13 @@
 <?php
-function TestIpPortState($IP,$Port,$ERROR_NO,$ERROR_STR)
-{
-	if($fp=fsockopen($IP,$Port,$ERROR_NO,$ERROR_STR,(float)0.5))
-	{
-		return true;
-	}else{
-		echo $ERROR_NO.":".$ERROR_STR;
-		return false;
-	}
-	return false;
-}
+    function TestIpPortState($IP,$Port,$ERROR_NO,$ERROR_STR)
+    {
+        if($fp=fsockopen($IP,$Port,$ERROR_NO,$ERROR_STR,(float)0.5))
+        {
+            return true;
+        }else{
+            echo $ERROR_NO.":".$ERROR_STR;
+            return false;
+        }
+        return false;
+    }
 ?>
--- index.php
+++ index.php
@@ -4,13 +4,12 @@ include("connect.php");
 include("function.php");
 include("message.php");
 
-echo "<html>";
-echo "<head>";
-echo "<link rel='shortcut icon' href='index.ico' type='image/ico'>";
-echo "</head>";
-echo "<body>";
+    echo "<html>";
+    echo "<head>";
+    echo "<link rel='shortcut icon' href='index.ico' type='image/ico'>";
+    echo "</head>";
+    echo "<body>";
 
-
-echo "</body>";
-echo "</html>";
+    echo "</body>";
+    echo "</html>";
 ?>
