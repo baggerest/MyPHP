d4f9ab40353f3d6b661d03c9bb97094d55748d92
baggerest <baggerest@yahoo.com.tw>
2017-01-22 14:33:36 +0800

    寫入class的變數取得

--- index.php
+++ index.php
@@ -12,4 +12,14 @@ include("message.php");
 
     echo "</body>";
     echo "</html>";
+
+    class zz {
+        var $ss = array(
+            's1' => '123',
+            's2' => '15'
+        );
+    }
+
+    $copy = new zz;
+    echo $copy->ss['s2'];
 ?>
