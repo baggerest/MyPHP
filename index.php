a781685f0b06d699b3804680812ac53d33282334
baggerest <baggerest@yahoo.com.tw>
2017-01-23 13:49:54 +0800

    更改成以html為主的檔案結構

--- index.php
+++ index.php
@@ -1,16 +1,19 @@
 <?php
-include("config.php");
-include("connect.php");
-include("function.php");
-include("message.php");
-include("allclass.php");
-
-    echo "<html>";
-    echo "<head>";
-    echo "<link rel='shortcut icon' href='index.ico' type='image/ico'>";
-    echo "</head>";
-    echo "<body>";
-    echo "<img src=index.ico>";
-    echo "</body>";
-    echo "</html>";
+    include("config.php");
+    include("connect.php");
+    include("function.php");
+    include("message.php");
+    include("allclass.php");
 ?>
+
+<html>
+    <head>
+        <link rel='shortcut icon' href='index.ico' type='image/ico'>
+        <title></title>
+    </head>
+    <body>
+    <div align=center>
+        <a href=<?php echo($_SERVER['PHP_SELF']);?>><img src=index.ico></a>
+    </div>
+    </body>
+</html>
