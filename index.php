f0d256ffe1ba3b02dcc826d5d727a3577bbd30ec
baggerest <baggerest@yahoo.com.tw>
2017-01-23 13:51:07 +0800

    更改成以html為主的檔案結構

--- index.php
+++ index.php
@@ -7,13 +7,16 @@
 ?>
 
 <html>
+
     <head>
         <link rel='shortcut icon' href='index.ico' type='image/ico'>
         <title></title>
     </head>
+
     <body>
-    <div align=center>
-        <a href=<?php echo($_SERVER['PHP_SELF']);?>><img src=index.ico></a>
-    </div>
+        <div align=center>
+            <a href=<?php echo($_SERVER['PHP_SELF']);?>><img src=index.ico></a>
+        </div>
     </body>
+
 </html>
