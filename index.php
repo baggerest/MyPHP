4bae9ebfbc392bd6c70989394939a2d64c9729f3
baggerest <baggerest@yahoo.com.tw>
2017-01-25 15:57:40 +0800

    加入時區選單

--- index.php
+++ index.php
@@ -14,13 +14,15 @@
         <title><?php echo $server['title'];?></title>
     </head>
     <body>
+    <div align="center">
+    </div>
         <div class="mainbtn">
             <a href=<?php echo($_SERVER['PHP_SELF']);?>><img src=index.ico style="width: 128px;height: 128px">
             <div class="btn"><?php echo $server['main'];?></div></a>
         </div>
         <div class="language">
             <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
-                <select name="YourLocation" onChange="changed(this);">
+                <select name="language_select" onChange="changed(this);">
                     <?php
                         foreach ($language as $value => $key){
                             $upper = strtoupper($value);
@@ -29,6 +31,22 @@
                             }else{
                                 echo "<option value={$upper}>{$key} / {$upper}</option>";
                             }
+                        }
+                    ?>
+                </select>
+                <select name="timezone_select">
+                    <?php
+                        $timeset = '';
+                        foreach ($timezone as $value){
+                            foreach ($value as $key => $item){
+                                if($key=='label'){
+                                    $timeset = $item;
+                                    echo "<optgroup label='".$item."'>";
+                                }else{
+                                    echo "<option title='".$timeset." - ".$item."' value='".$key."'>".$item."</option>";
+                                }
+                            }
+                            echo "</optgroup>";
                         }
                     ?>
                 </select>
